c8958884efc77e99f97d3cb54fc641ce81453ee8
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-03-14 20:55:18 +0900

    chessboard solved!

--- baekjoon/25682_prefixsum.py
+++ baekjoon/25682_prefixsum.py
@@ -1,24 +1,24 @@
 import sys
 input = sys.stdin.readline
 n, m, k = map(int, input().split(' '))
-p_1 = [[0]*(n+1) for _ in range(m+1)]
+p = [[0]*(m+1) for _ in range(n+1)]
 
-for i in range(1, m+1):
+for i in range(1, n+1):
     arr = list(input())
-    for j in range(1, n+1):
+    for j in range(1, m+1):
         if (i+j)%2 == 0 and arr[j-1] == 'B':
-            p_1[i][j] = p_1[i-1][j] + p_1[i][j-1] - p_1[i-1][j-1]
+            p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1]
         elif (i+j)%2 == 1 and arr[j-1] == 'W':
-            p_1[i][j] = p_1[i-1][j] + p_1[i][j-1] - p_1[i-1][j-1]
+            p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1]
         else:
-            p_1[i][j] = p_1[i-1][j] + p_1[i][j-1] - p_1[i-1][j-1] + 1
+            p[i][j] = p[i-1][j] + p[i][j-1] - p[i-1][j-1] + 1
     
 
 min_ = float('inf')
 max_ = -float('inf')
 for r in range(k, n+1):
     for c in range(k, m+1):
-        min_ = min(p_1[r][c] - p_1[r-k][c] - p_1[r][c-k] + p_1[r-k][c-k], min_)
-        max_ = max(p_1[r][c] - p_1[r-k][c] - p_1[r][c-k] + p_1[r-k][c-k], max_)
+        min_ = min(p[r][c] - p[r-k][c] - p[r][c-k] + p[r-k][c-k], min_)
+        max_ = max(p[r][c] - p[r-k][c] - p[r][c-k] + p[r-k][c-k], max_)
 
-print(min(min_, k*k-max_))
+print(min(min_, max_, k*k-min_, k*k-max_))
