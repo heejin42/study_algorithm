215e56b5f37000363b14a04dfa54edf980b34be4
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-06-22 19:10:22 +0900

    programmers heap solved

--- programmers/[heap]heap_command.py
+++ programmers/[heap]heap_command.py
@@ -1,23 +1,20 @@
 import heapq
+
 def solution(operations):
     heap = []
     for op in operations:
-        if op[0] == 'I':
-            num = int(op[2:])
-            heapq.heappush(heap, num)
+        command, num = op.split(' ')
+        if command == 'I':
+            heapq.heappush(heap, int(num))
         else:
             if len(heap) == 0:
                 continue
-            if op[2:] == '1':
-                heap.remove(max(heap))
-            elif op[2:] == '-1':
-                heap.remove(min(heap))
+            elif num == '1':
+                heap.pop()
+            elif num == '-1':
+                heap.pop(0)
     if len(heap) == 0:
         answer = [0, 0]
     else:
         answer = [max(heap), min(heap)]
     return answer
-
-
-operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
-print(solution(operations))
