3274a51e154f7ad33e1a94e882ade12620d7f2dd
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-09-04 15:30:01 +0900

    hackerrank: strings minion game solved

---
+++ hackerrank/minion_game.py
@@ -0,0 +1,30 @@
+def minion_game(string):
+    # kevin and stuart game do this with string S
+    # stuart start with consonants (자음)
+    # kevin start with vowels (모음-aeiou)
+    # make all possible substirngs
+    # each substrings have point
+    # them, output is the total score and winner info
+    # example Stuart 12
+    length = len(string)
+    vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+    score_k = 0
+    score_s = 0
+    for i in range(len(string)):
+        x = string[i]
+        if x in vowels:
+            score_k += length - i 
+        else:
+            score_s += length - i
+            
+    if score_k > score_s:
+        result = "Kevin " + str(score_k)
+    elif score_k < score_s:
+        result = "Stuart " + str(score_s)
+    else:
+        res
+    return result
+ 
+string = 'BANANA'
+print(minion_game(string))    
+     
