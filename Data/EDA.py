0a0122e4ec2cf4675fd1a46a05ecf1deb8ec1299
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-09-13 18:18:25 +0900

    230913 eda studying

--- Data/EDA.py
+++ Data/EDA.py
@@ -4,4 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 stock_prices = pd.read_csv("World-Stock-Prices-Dataset.csv")
-print(stock_prices)
+
+stock_prices["Dividends"] = stock_prices["Dividends"].astype(object)
+stock_prices["Stock Splits"] = stock_prices["Stock Splits"].astype(object)
+
+missing_df = stock_prices.isnull().sum().reset_index()
+missing_df.columns = ['column', 'count']
+missing_df['ratio'] = missing_df['count'] / stock_prices.shape[0]
+
+nexflix_stock = stock_prices[stock_prices['Brand_Name'] == "netflix"]
+
+sns.distplot(nexflix_stock['Open'])
+plt.show()
