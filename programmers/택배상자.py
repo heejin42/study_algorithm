abbe6d42c01401539605993a12c9c92b16a0a127
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-09-14 19:08:39 +0900

    programmers: stack and queue level2 solved

---
+++ programmers/택배상자.py
@@ -0,0 +1,27 @@
+from collections import deque
+def solution(order):
+    # 배달하는 순서에 맞게 택배를 실어야 한다.
+    # 해당 택배를 실을 순서가 될 때까지 다른 곳에 보관해야 하므로 보조 컨테이너 벨트를 설치한다. 
+    # 스택의 구조, 가장 마지막의 보관한 상자만 뺄 수 있다.
+    # 몇개의 상자를 실을 수 있는지 
+    q = deque(order)
+    stack = []
+    i = 1
+    while q:
+        if i == q[0]:
+            q.popleft()
+        elif len(stack) > 0 and stack[-1] == q[0]:
+            stack.pop()
+            q.popleft()
+            continue
+        else:
+            if i > len(order) and stack[-1]!=q[0]:
+                break
+            else:
+                stack.append(i)
+        i += 1
+    answer = len(order)-len(q)
+    return answer
+
+order = [5,4,3,2,1]
+print(solution(order))
