9fb0d6d981be6c5f5c10c36b5712adf7c41e2fcf
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-09-21 16:07:15 +0900

    EDA: subplots

--- Data/EDA.py
+++ Data/EDA.py
@@ -38,10 +38,39 @@ stock_prices = pd.read_csv("World-Stock-Prices-Dataset.csv")
 #         autopct = "%.2f%%")
 # plt.show()
 
-stock_data = stock_prices[stock_prices.Industry_Tag.isin(["technology", "entertainment", "social media", "apparel", "fitness", "footwear", "food", "food & beverage"])]
-# sns.barplot(x = 'Industry_Tag', y = 'High', data=stock_data)
-# sns.pointplot(x='Industry_Tag', y = 'High', data=stock_data
-# sns.boxplot(x='Industry_Tag', y='High', data=stock_data)
-food_stock = stock_prices[stock_prices.Industry_Tag.isin(["food", "food & beverage"])]
-sns.violinplot(x="Brand_Name", y="High", hue="Industry_Tag", data=food_stock)
+#stock_data = stock_prices[stock_prices.Industry_Tag.isin(["technology", "entertainment", "social media", "apparel", "fitness", "footwear", "food", "food & beverage"])]
+# print(stock_data['Brand_Name'].unique())
+# # sns.barplot(x = 'Industry_Tag', y = 'High', data=stock_data)
+# # sns.pointplot(x='Industry_Tag', y = 'High', data=stock_data)
+# # sns.boxplot(x='Industry_Tag', y='High', data=stock_data)
+# food_stock = stock_prices[stock_prices.Industry_Tag.isin(["food", "food & beverage"])]
+# sns.violinplot(x="Brand_Name", y="High", hue="Industry_Tag", data=food_stock)
+# plt.show()
+
+
+# subplots 실습
+
+stock_data = stock_prices[stock_prices.Country.isin(["usa", "japan"])]
+stock_data = stock_data[stock_data.Industry_Tag.isin(['gaming', 'automotive'])]
+
+fig, axs = plt.subplots(2, 2, figsize=(8,8))
+ax1, ax2, ax3, ax4 = axs.flatten()
+
+ax1 = sns.boxplot(x='Industry_Tag', y='High', data=stock_data, ax=ax1)
+ax2 = sns.barplot(x = 'Industry_Tag', y = 'High', data=stock_data, ax=ax2)
+ax3 = sns.pointplot(x='Industry_Tag', y = 'High', data=stock_data, ax=ax3)
+ax4 = sns.violinplot(x='Industry_Tag', y = 'High', hue="Country", data=stock_data, ax=ax4)
+
+ax1.set_title("Stock price distribution by industry")
+ax2.set_title("Average stock price by industry")
+ax3.set_title("Average stock price by industry")
+ax4.set_title("Stock price distribution by industry(usa,japan)")
+# y축 이름 설정 추가
+ax2.set_ylabel('average of High prices')
+# y축 눈금 범위 변경
+ax4.set_yticks(np.arange(0, 350, 50))
+# 전체타이틀 & 그래프 사이 간격 조정
+fig.suptitle("Stock prices data visualization")
+fig.tight_layout()
+
 plt.show()
