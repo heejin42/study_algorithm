31803daeee371195c19fdfd56622c4fe99a3f096
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-10-11 14:36:55 +0900

    prgrammers: level2 solved

---
+++ programmers/요격시스템.py
@@ -0,0 +1,20 @@
+def solution(targets):
+    answer = 1
+    targets.sort(key = lambda x:x[1])
+    print(targets)
+    l = targets[0][0] 
+    r = targets[0][1]
+    for target in targets:
+        if target[0] <= l:
+            continue
+        elif l < target[0] < r:
+            l = target[0] 
+            continue
+        else:
+            answer += 1
+            l = target[0]
+            r = target[1]    
+    return answer
+
+targets = [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]
+print(solution(targets))
