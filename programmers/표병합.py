3190a0240cccf21988ced6e9cd192932e9f4e2f5
heejin42 <81953581+heejin42@users.noreply.github.com>
2023-11-13 22:26:33 +0900

    programmers: chart merge level3 solution

---
+++ programmers/표병합.py
@@ -0,0 +1,53 @@
+# 표의 크기 - 50 x 50
+# 초기 셀은 비어있다
+# UPDATE r c value
+# UPDATE value1 value2
+# MERGE r1 c1 r2 c2 (r1, c1 값을 가진다)
+# UNMERGE r c -> (r,c 위치의 모든 병합 해제 r,c는 값을 가지고 나머지는 초기로)
+# PRINT r c
+
+def solution(commands):
+    answer = []
+    merged = [[(i, j) for j in range(50)] for i in range(50)]
+    board = [["EMPTY"] * 50 for _ in range(50)]
+    for command in commands:
+        command = command.split(' ')
+        if command[0] == 'UPDATE':
+            if len(command) == 4:
+                r,c,value = int(command[1])-1,int(command[2])-1,command[3]
+                x,y = merged[r][c]
+                board[x][y] = value
+            elif len(command) == 3:
+                value1, value2 = command[1], command[2]
+                for i in range(50):
+                    for j in range(50):
+                        if board[i][j] == value1:
+                            board[i][j] = value2
+        elif command[0] == 'MERGE':
+            r1,c1,r2,c2 = int(command[1])-1, int(command[2])-1, int(command[3])-1, int(command[4])-1
+            x1,y1 = merged[r1][c1]
+            x2,y2 = merged[r2][c2]
+            if board[x1][y1] == "EMPTY":
+                board[x1][y1] = board[x2][y2]
+            for i in range(50):
+                for j in range(50):
+                    if merged[i][j] == (x2,y2):
+                        merged[i][j] = (x1,y1)
+        elif command[0] == 'UNMERGE':
+            r, c = int(command[1])-1,int(command[2])-1
+            x, y = merged[r][c]
+            tmp = board[x][y]
+            for i in range(50):
+                for j in range(50):
+                    if merged[i][j] == (x,y):
+                        merged[i][j] = (i,j)
+                        board[i][j] = "EMPTY"
+            board[r][c] = tmp
+        elif command[0] == 'PRINT':
+            r, c = int(command[1])-1, int(command[2])-1
+            x, y = merged[r][c]
+            answer.append(board[x][y])
+    return answer
+            
+commands = ["UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"]
+print(solution(commands))
