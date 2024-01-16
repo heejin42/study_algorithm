1d915f0c53513201e486141b0cf564d1256fd739
heejin42 <81953581+heejin42@users.noreply.github.com>
2024-01-16 16:03:32 +0900

    baekjoon: dp solved

--- baekjoon/DP/14916_거스름돈.py
+++ baekjoon/DP/14916_거스름돈.py
@@ -1,19 +1,24 @@
-# 2원짜리와 5원짜리 거스름돈이 있다. 동전 개수 최소를 구해라
-n = int(input())
-if n < 6:
-    if n == 2 or n == 5:
-        print(1)
-    if n == 4:
-        print(2)
-    print(-1)
+import sys
+input = sys.stdin.readline
 
-dp = [n for _ in range(n+1)]
-dp[2] = 1
-dp[4] = 2
-dp[5] = 1
-for i in range(6, n+1):
-    dp[i] = min(dp[i], dp[i-2] + 1, dp[i-5] + 1)
-if dp[n] == n:
-    print(-1)
-else:
-    print(dp[n])
+def sol(money):
+    cnt = 0
+
+    while money > 0:
+        if money % 5 == 0:
+            cnt += money//5
+            break
+        else:
+            money -= 2
+            cnt += 1
+
+    if money < 0:
+        print(-1)
+    else:
+        print(cnt)
+
+
+if __name__ == '__main__':
+    money = int(input())
+
+    sol(money)
