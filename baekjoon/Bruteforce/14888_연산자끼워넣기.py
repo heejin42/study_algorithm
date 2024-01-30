8fb3a318d7d21d5c07fab005fdef2b45f8ae5b5d
heejin42 <81953581+heejin42@users.noreply.github.com>
2024-01-30 19:08:33 +0900

    baekjoon 14888 fixed

--- baekjoon/Bruteforce/14888_연산자끼워넣기.py
+++ baekjoon/Bruteforce/14888_연산자끼워넣기.py
@@ -1,44 +1,28 @@
-# n개의 수와 N-1개의 연산자가 주어졌을 때, 
-# 만들 수 있는 식의 결과가 최대인 것과 최소인 것을 구하라
-# 백트래킹 풀이 참고 - https://velog.io/@kimdukbae/BOJ-14888-%EC%97%B0%EC%82%B0%EC%9E%90-%EB%81%BC%EC%9B%8C%EB%84%A3%EA%B8%B0-Python
-import sys
 from itertools import permutations
+n = int(input())
+number = list(map(int, input().split()))
+op = list(map(int, input().split()));
+operator = '+' * op[0] + '-' * op[1] + '*' * op[2] + '/' * op[3]
+operator_perm = permutations(operator, n-1)
 
-input = sys.stdin.readline
-N = int(input())
-num = list(map(int, input().split()))
-op_num = list(map(int, input().split()))  # +, -, *, /
-op_list = ['+', '-', '*', '/']
-op = []
+max_result = - int(1e9)
+min_result = int(1e9)
+for perm in operator_perm:
+    result = number[0]
+    for i in range(1, n):
+        if perm[i-1] == '+':
+            result += number[i]
+        elif perm[i-1] == '-':
+            result -= number[i]
+        elif perm[i-1] == '*':
+            result *= number[i]
+        elif perm[i-1] == '/':
+            if result < 0 and number[i] > 0: 
+                result = -1*( result*(-1) // number[i])
+            else:
+                result //= number[i]
+    max_result = max(max_result, result)
+    min_result = min(min_result, result)
 
-for k in range(len(op_num)):
-    for i in range(op_num[k]):
-        op.append(op_list[k])
-
-maximum = -1e9
-minimum = 1e9
-
-
-def solve():
-    global maximum, minimum
-    for case in permutations(op, N - 1):
-        total = num[0]
-        for r in range(1, N):
-            if case[r - 1] == '+':
-                total += num[r]
-            elif case[r - 1] == '-':
-                total -= num[r]
-            elif case[r - 1] == '*':
-                total *= num[r]
-            elif case[r - 1] == '/':
-                total = int(total / num[r])
-
-        if total > maximum:
-            maximum = total
-        if total < minimum:
-            minimum = total
-
-
-solve()
-print(maximum)
-print(minimum)
+print(max_result)
+print(min_result)
